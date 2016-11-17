35d59e7eccf6108fe4d080cbced9633c599696da
Hao-Han Chao <jimmy1993316@D-10-156-27-85.dhcp4.washington.edu>
2016-11-17 15:21:34 -0800

    Add second and third activity
    1. click search button that call exercise list activity
    2. click list item that call exercise detail activity

---
+++ app/src/main/java/com/projectomega/workoutsource/ExerciseList.java
@@ -0,0 +1,37 @@
+package com.projectomega.workoutsource;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView.OnItemClickListener;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+
+public class ExerciseList extends AppCompatActivity {
+
+    public ListView listView;
+    private String[] workoutArray = {"a", "b", "c", "d"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_list);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_listview, workoutArray);
+
+        listView = (ListView) findViewById(R.id.workout_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?>adapter, View v, int position, long id){
+                Object item = adapter.getItemAtPosition(position);
+
+                Intent intent = new Intent(ExerciseList.this,ExerciseDetail.class);
+                //based on item add info to intent
+                startActivity(intent);
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/projectomega/workoutsource/MainActivity.java
+++ app/src/main/java/com/projectomega/workoutsource/MainActivity.java
@@ -1,7 +1,9 @@
 package com.projectomega.workoutsource;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void queryWorkout(View view) {
+        Intent intent = new Intent(this, ExerciseList.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
