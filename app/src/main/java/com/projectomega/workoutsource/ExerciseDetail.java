75303253559fbdbd47a985493de55e5eb8b1ff1b
Henry Chao <hhc438@uw.edu>
2016-12-02 16:38:46 -0800

    Fix Bugs

--- app/src/main/java/com/projectomega/workoutsource/ExerciseDetail.java
+++ app/src/main/java/com/projectomega/workoutsource/ExerciseDetail.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -30,7 +31,6 @@ public class ExerciseDetail extends AppCompatActivity {
     private List<String> bodyparts; // 7
     private List<String> injuries; // 8
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -46,42 +46,40 @@ public class ExerciseDetail extends AppCompatActivity {
         setContentView(R.layout.activity_exercise_detail);
         setTitle(R.string.exercise_detail);
 
-        TextView t = (TextView)findViewById(R.id.nameTextView);
-        if (data[0] != null && data[0] instanceof String) // name
-            t.setText(data[0].toString());
-        t = (TextView)findViewById(R.id.difficultyTextView);
-        if (data[1] != null && data[1] instanceof BigDecimal) // difficulty
-            t.setText(data[1].toString());
-        t = (TextView)findViewById(R.id.warningTextView);
-        if (data[2] != null && data[1] instanceof String) // warning
-            t.setText(data[2].toString());
-
         new DownloadImageTask((ImageView) findViewById(R.id.workoutImageView))
                 .execute(data[3].toString());
 
-        t = (TextView)findViewById(R.id.descriptionTextView);
-        if (data[4] != null && data[4] instanceof String) // description
-            t.setText(data[4].toString());
-        t = (TextView)findViewById(R.id.directionTextView);
-        if (data[5] != null && data[5] instanceof String) // direction
-            t.setText(data[5].toString());
-        t = (TextView)findViewById(R.id.equipmentsTextView);
-        if (data[6] != null && data[6] instanceof List) // description
-            t.setText(data[6].toString());
-        t = (TextView)findViewById(R.id.bodyPartsTextView);
-        if (data[7] != null && data[7] instanceof List) // direction
-            t.setText(data[7].toString());
-
-        System.out.println(data[0]);
-        System.out.println(data[1]);
-        System.out.println(data[2]);
-        System.out.println(data[3]);
-        System.out.println(data[4]);
-        System.out.println(data[5]);
-        System.out.println(data[6]);
-        System.out.println(data[7]);
-        //System.out.println(data[8]); ArrayIndexOutOfBoundsException: length=8; index =8
-        System.out.println("ExerciseDetail.java -- display() DISPLAY STUFF that's in the Data array");
+        TextView t = null;
+        for (int i = 0; i < data.length && data != null; i++)
+        {
+            switch(i)
+            {
+                case 0:
+                    t = (TextView)findViewById(R.id.nameTextView);
+                    break;
+                case 1:
+                    t = (TextView)findViewById(R.id.difficultyTextView);
+                    break;
+                case 2:
+                    t = (TextView)findViewById(R.id.warningTextView);
+                    break;
+                case 4:
+                    t = (TextView)findViewById(R.id.descriptionTextView);
+                    break;
+                case 5:
+                    t = (TextView)findViewById(R.id.directionTextView);
+                    break;
+                case 6:
+                    t = (TextView)findViewById(R.id.equipmentsTextView);
+                    break;
+                case 7:
+                    t = (TextView)findViewById(R.id.bodyPartsTextView);
+                    break;
+            }
+            if ((data[i] != null) && (data[i] instanceof String ||
+                    data[i] instanceof BigDecimal || data[i] instanceof List) && (i != 3))
+                t.setText(data[i].toString());
+        }
     }
 
     public void queryDetails (String exName) {
